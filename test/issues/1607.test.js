dd12a31902667402d6c0ddbeb9347e871ad6c339
Sherry Ma <sherry.hyma@gmail.com>
2020-04-01 13:16:38 -0400

    added one test case

---
+++ test/issues/1607.test.js
@@ -0,0 +1,31 @@
+const {initializeWithTab} = require("../common");
+
+describe("#1607", function () {
+  describe("when the user has one window and one container", function () {
+    it("should not have any action and only have one window", async function () {
+      const webExtension = await initializeWithTab({
+        cookieStoreId: "firefox-container-1",
+        url: "http://example.com"
+      });
+
+      await webExtension.popup.helper.clickElementById("container-page-assigned");
+
+      const responses = {};
+      await webExtension.background.browser.tabs._create({
+        url: "https://example.com"
+      }, {
+        options: {
+          webRequestRedirects: ["https://example.com"],
+          webRequestError: true,
+          instantRedirects: true
+        },
+        responses
+      });
+
+      webExtension.browser.tabs.move.should.have.been.not.calledOnce;
+      
+
+      webExtension.destroy();
+    });
+  });
+});
